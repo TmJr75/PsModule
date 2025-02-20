d3a17d896f0865711ab5fe3f79d31b6dd79706a2
Tore Melberg <tore@melberg.net>
2025-02-20 16:23:45 +0100

    updated files

--- Public/New-PsModule.ps1
+++ Public/New-PsModule.ps1
@@ -3,12 +3,65 @@
 # New-Module -Name "PSModule" 
 Function New-PsModule {
 
+
+
+  <#
+    .SYNOPSIS
+    Create a new PowerShell module with a manifest file.
+
+    .DESCRIPTION
+    This function will create a new PowerShell module with a manifest file. The module will be created in the specified folder.
+
+    .PARAMETER moduleName
+    The name of the module to create.
+
+    .PARAMETER Author
+    The author of the module.
+
+    .PARAMETER CompanyName
+    The name of the company that owns the module.
+
+    .PARAMETER PowerShellVersion
+    The version of PowerShell that the module is compatible with.
+
+    .PARAMETER PowerShellHostVersion
+    The version of the PowerShell host that the module is compatible with.
+
+    .PARAMETER FunctionsToExport
+    The functions to export from the module.
+
+    .PARAMETER CmdletsToExport
+    The cmdlets to export from the module.
+
+    .PARAMETER VariablesToExport
+    The variables to export from the module.
+
+    .PARAMETER AliasesToExport
+    The aliases to export from the module.
+
+    .PARAMETER ModuleVersion
+    The version of the module.
+
+    .PARAMETER sourceFolder
+    The folder where the module will be created.
+
+    .EXAMPLE
+    New-PsModule -moduleName "MyModule" -Author "John Doe" -CompanyName "Acme Corp" -PowerShellVersion "5.1" -PowerShellHostVersion "5.1" -FunctionsToExport "*" -CmdletsToExport "*" -VariablesToExport "*" -AliasesToExport "*" -ModuleVersion 1.0.0.0 -sourceFolder "C:\Temp\"
+    This example creates a new module named "MyModule" in the "C:\Temp\" folder.
+
+    .NOTES
+    File Name      : New-PsModule.ps1
+    Author         : Tore Melberg
+    
+  #>
+
     [CmdletBinding()]
     Param(
         [string]$moduleName,
         [string]$Author,
         [string]$CompanyName,
-        [string]$PowerShellVersion,
+        [string]$PowerShellVersion = "5.1,7.4",
+        [string]$PowerShellHostVersion = "",
         [string]$FunctionsToExport = "*",
         [string]$CmdletsToExport = "*", 
         [string]$VariablesToExport = "*", 
@@ -36,7 +89,7 @@ Foreach ($f in $tempFolders) {
 
 
 
-    New-Item -Path "$sourcefolder\$moduleName" -Name "$moduleName.ps1" -ItemType File -Value 'Get-ChildItem -Path $PSScriptRoot\*.ps1 -Exclude *.tests.ps1, *profile.ps1 -Recurse | 
+    New-Item -Path "$sourcefolder\$moduleName" -Name "$moduleName.psm1" -ItemType File -Value 'Get-ChildItem -Path $PSScriptRoot\*.ps1 -Exclude *.tests.ps1, *profile.ps1 -Recurse | 
         ForEach-Object {
 	    . $_.FullName
     }' -Force
@@ -46,9 +99,11 @@ New-ModuleManifest -Path "$sourcefolder\$moduleName\$moduleName.psd1" `
     -Author $Author `
     -CompanyName $CompanyName `
     -PowerShellVersion $PowerShellVersion `
+    -PowerShellHostVersion $PowerShellHostVersion `
     -FunctionsToExport $FunctionsToExport `
     -CmdletsToExport $CmdletsToExport `
     -VariablesToExport $VariablesToExport `
-    -AliasesToExport $AliasesToExport
+    -AliasesToExport $AliasesToExport `
+    -RootModule ("$moduleName" + ".psm1")
 }
 
